7a8d2216bf34c0bfd32d3cbde2d62a083b351e61
Brent <brentbaughan1@gmail.com>
2021-04-22 12:52:42 -0400

    created modular question lists

--- index.js
+++ index.js
@@ -1,7 +1,80 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const formatResponses = require('');
-const questions = [];
+const initialQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: `Enter the team manager's name here.`
+    },
+    {
+        type: 'input',
+        name: 'managerEmployeeID',
+        message: `Enter the team manager's employee ID here.`
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: `Enter the team manager's e-mail address here.`
+    },
+    {
+        type: 'input',
+        name: 'managerOffice',
+        message: `Enter the team manager's office number here.`
+    }
+];
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: `Enter the engineer's name here.`
+    },
+    {
+        type: 'input',
+        name: 'engineerEmployeeID',
+        message: `Enter the engineer's employee ID here.`
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: `Enter the engineer's e-mail address here.`
+    },
+    {
+        type: 'input',
+        name: 'engineerOffice',
+        message: `Enter the engineer's office number here.`
+    }
+];
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: `Enter the intern's name here.`
+    },
+    {
+        type: 'input',
+        name: 'internID',
+        message: `Enter the intern's ID here.`
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: `Enter the intern's e-mail address here.`
+    },
+    {
+        type: 'input',
+        name: 'internSchool',
+        message: `Enter the intern's school here.`
+    }
+];
+const promptMore = [
+    {
+        type: 'list',
+        name: 'moreResponse',
+        choices: ["Add an intern", "Add an engineer", "No, my team has been entered!"],
+        message: `Do you want to add any more team members?`
+    }
+];
 
 
 
